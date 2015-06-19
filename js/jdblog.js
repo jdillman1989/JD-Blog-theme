01c98c06491da8aa5ccef16669159d196ef195d5
Jesse Dillman <JDillman@bibleleague.org>
2015-06-19 10:23:20 -0500

    test run

--- js/jdblog.js
+++ js/jdblog.js
@@ -1,153 +1,153 @@
-jQuery(document).ready(function() {
+// jQuery(document).ready(function() {
 
-	// sticky sidebar
+// 	// sticky sidebar
 
-	var sidebar = jQuery(".sidebar");
-	var content = jQuery(".content");
-	var sidebarScroll = "sidebar-scrolled";
-	var sidebarEndScroll = "sidebar-end-scrolled";
-	var navHeight = jQuery('.nav').height();
-	var sliderHeight = jQuery('.slider').height();
-	var prevHeight = headerHeight + navHeight + sliderHeight;
+// 	var sidebar = jQuery(".sidebar");
+// 	var content = jQuery(".content");
+// 	var sidebarScroll = "sidebar-scrolled";
+// 	var sidebarEndScroll = "sidebar-end-scrolled";
+// 	var navHeight = jQuery('.nav').height();
+// 	var sliderHeight = jQuery('.slider').height();
+// 	var prevHeight = headerHeight + navHeight + sliderHeight;
 
-	var containerHeight = jQuery('.content-container').height();
-	var documentHeight = jQuery(document).height();
-	var footerHeight = jQuery('.footer').outerHeight();
-	var windowHeight = jQuery(window).height();
-	var sidebarHeight = jQuery('.sidebar').height();
+// 	var containerHeight = jQuery('.content-container').height();
+// 	var documentHeight = jQuery(document).height();
+// 	var footerHeight = jQuery('.footer').outerHeight();
+// 	var windowHeight = jQuery(window).height();
+// 	var sidebarHeight = jQuery('.sidebar').height();
 
-	function currentSidebarPos(){
+// 	function currentSidebarPos(){
 
-		// 105 includes 40px sidebar padding and 65px sidebar top
-		return jQuery(window).scrollTop() + sidebarHeight + 105;
-	}
+// 		// 105 includes 40px sidebar padding and 65px sidebar top
+// 		return jQuery(window).scrollTop() + sidebarHeight + 105;
+// 	}
 
-	function footerStartPos(){
+// 	function footerStartPos(){
 
-		// includes 20px container margin
-		return jQuery(document).height() - footerHeight - 20;
-	}
+// 		// includes 20px container margin
+// 		return jQuery(document).height() - footerHeight - 20;
+// 	}
 
-	function fixedSidebar(){
+// 	function fixedSidebar(){
 
-		sidebar.css({ 
-			'position': 'fixed',
-			'top': 65,
-			'left': sidebar.offset().left,
-			'margin-top': 0
-		});
-	}
+// 		sidebar.css({ 
+// 			'position': 'fixed',
+// 			'top': 65,
+// 			'left': sidebar.offset().left,
+// 			'margin-top': 0
+// 		});
+// 	}
 
-	function staticSidebar(){
+// 	function staticSidebar(){
 
-		sidebar.css({ 
-			'position': 'static',
-			'top': 'auto',
-			'left': 'auto',
-			'margin-top': 0
-		});
-	}
+// 		sidebar.css({ 
+// 			'position': 'static',
+// 			'top': 'auto',
+// 			'left': 'auto',
+// 			'margin-top': 0
+// 		});
+// 	}
 
-	var lastScrollTop = 0, delta = 5;
+// 	var lastScrollTop = 0, delta = 5;
 
-	jQuery(window).scroll(function() {
+// 	jQuery(window).scroll(function() {
 
-		// Only run on desktop
-		if (jQuery(window).width() > 676 && sidebar.height() < content.height() + 50) {
+// 		// Only run on desktop
+// 		if (jQuery(window).width() > 676 && sidebar.height() < content.height() + 50) {
 
-			// Check if scrolled to sidebar top
-			if( jQuery(this).scrollTop() > prevHeight + 20 ) {
+// 			// Check if scrolled to sidebar top
+// 			if( jQuery(this).scrollTop() > prevHeight + 20 ) {
 
-				var st = jQuery(window).scrollTop();
+// 				var st = jQuery(window).scrollTop();
 
-				if(Math.abs(lastScrollTop - st) <= delta)
-				  return;
+// 				if(Math.abs(lastScrollTop - st) <= delta)
+// 				  return;
 
-				if (st > lastScrollTop){
-					// downscroll code
-					console.log('scroll down');
-				} 
+// 				if (st > lastScrollTop){
+// 					// downscroll code
+// 					console.log('scroll down');
+// 				} 
 
-				else {
-					// upscroll code
-					console.log('scroll up');
-				}
+// 				else {
+// 					// upscroll code
+// 					console.log('scroll up');
+// 				}
 
-				lastScrollTop = st;
+// 				lastScrollTop = st;
 
-				fixedSidebar();
+// 				fixedSidebar();
 
-				// Check if window is smaller than sidebar
-				if( jQuery(window).height() < sidebar.height() ) {
-					console.log(" small screen ");
-				}
+// 				// Check if window is smaller than sidebar
+// 				if( jQuery(window).height() < sidebar.height() ) {
+// 					console.log(" small screen ");
+// 				}
 
-				// Check if scrolled to footer
-				if( currentSidebarPos() > footerStartPos()) {
+// 				// Check if scrolled to footer
+// 				if( currentSidebarPos() > footerStartPos()) {
 
-					sidebar.css({ 
-						'margin-top': footerStartPos() - currentSidebarPos()
-					});
-				};
-			} 
-			else {
+// 					sidebar.css({ 
+// 						'margin-top': footerStartPos() - currentSidebarPos()
+// 					});
+// 				};
+// 			} 
+// 			else {
 
-				staticSidebar();
-			}
-		};
-	});
+// 				staticSidebar();
+// 			}
+// 		};
+// 	});
 
-	jQuery(window).resize(function() {
+// 	jQuery(window).resize(function() {
 
-		// Only run on desktop
-		if (jQuery(window).width() > 676 && sidebar.height() < content.height() + 50) {
+// 		// Only run on desktop
+// 		if (jQuery(window).width() > 676 && sidebar.height() < content.height() + 50) {
 
-			// Check if scrolled to sidebar top
-			if( jQuery(this).scrollTop() > prevHeight + 20 ) {
+// 			// Check if scrolled to sidebar top
+// 			if( jQuery(this).scrollTop() > prevHeight + 20 ) {
 
-				sidebar.css({ 
-					'position': 'static',
-					'margin-top': jQuery(window).scrollTop() - sidebar.outerHeight()
-				});
+// 				sidebar.css({ 
+// 					'position': 'static',
+// 					'margin-top': jQuery(window).scrollTop() - sidebar.outerHeight()
+// 				});
 
-				// Check if scrolled to footer
-				if( currentSidebarPos() > footerStartPos()) {
+// 				// Check if scrolled to footer
+// 				if( currentSidebarPos() > footerStartPos()) {
 
-					sidebar.css({ 
-						'position': 'static',
-						'margin-top': (footerStartPos() - currentSidebarPos()) + (jQuery(window).scrollTop() - sidebar.outerHeight())
-					});
-				};
-			} 
-			else {
+// 					sidebar.css({ 
+// 						'position': 'static',
+// 						'margin-top': (footerStartPos() - currentSidebarPos()) + (jQuery(window).scrollTop() - sidebar.outerHeight())
+// 					});
+// 				};
+// 			} 
+// 			else {
 
-				staticSidebar();
-			}
-		}
-		else{
+// 				staticSidebar();
+// 			}
+// 		}
+// 		else{
 
-			staticSidebar();
-		}
-	});
+// 			staticSidebar();
+// 		}
+// 	});
 
-	// mobile menu (can't have any other inline style attrs on #menu-primary)
-	jQuery(function() {
-		var pull        = jQuery('#menuToggle');
-			menu        = jQuery('#menu-primary-1');
-			menuHeight  = menu.height();
+// 	// mobile menu (can't have any other inline style attrs on #menu-primary)
+// 	jQuery(function() {
+// 		var pull        = jQuery('#menuToggle');
+// 			menu        = jQuery('#menu-primary-1');
+// 			menuHeight  = menu.height();
 	 
-		jQuery(pull).on('click', function(e) {
-			e.preventDefault();
-			menu.slideToggle();
-		});
+// 		jQuery(pull).on('click', function(e) {
+// 			e.preventDefault();
+// 			menu.slideToggle();
+// 		});
 
-		jQuery(window).resize(function(){
+// 		jQuery(window).resize(function(){
 
-			var w = jQuery(window).width();
+// 			var w = jQuery(window).width();
 			
-			if(w > 676 && menu.is(':hidden')) {
-				menu.removeAttr('style');
-			}
-		});
-	});
-});
+// 			if(w > 676 && menu.is(':hidden')) {
+// 				menu.removeAttr('style');
+// 			}
+// 		});
+// 	});
+// });
