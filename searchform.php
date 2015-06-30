f14893a942205403df13d40701bb7405e311de89
Jesse Dillman <JDillman@bibleleague.org>
2015-06-30 13:59:49 -0500

    search style

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<div>
 		<input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" class="search-field" />
-		<input type="submit" id="searchsubmit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ); ?>" />
+		<input type="submit" id="searchsubmit" class="search-submit" />
 	</div>
 </form>
