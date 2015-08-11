b3b36a06d89f8452687e03062ec41a1f13eaaca9
Jesse Dillman <JDillman@bibleleague.org>
2015-08-11 14:53:38 -0500

    ajax form

--- functions.php
+++ functions.php
@@ -41,55 +41,43 @@ add_action( 'wp_ajax_mail_form', 'form_mail' );
 
 function form_mail() {
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	    // Get the form fields and remove whitespace.
-	    $name = strip_tags(trim($_POST["name"]));
+		// Get the form fields and remove whitespace.
+		$name = strip_tags(trim($_POST["name"]));
 				$name = str_replace(array("\r","\n"),array(" "," "),$name);
-	    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-	    $message = trim($_POST["message"]);
+		$email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+		$message = trim($_POST["message"]);
 
-	    // Check that data was sent to the mailer.
-	    if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-	        // Set a 400 (bad request) response code and exit.
-	        http_response_code(400);
-	        echo "Oops! There was a problem with your submission. Please complete the form and try again.";
-	        die();
-	    }
+		// Check that data was sent to the mailer.
+		if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+			// Set a 400 (bad request) response code and exit.
+			http_response_code(400);
+			echo "Oops! There was a problem with your submission. Please complete the form and try again.";
+			die();
+		}
+		else{
 
-	    // Set the recipient email address.
-	    $recipient = "jesse@jdillman.com";
+			// Set the recipient email address.
+			$file = "../../site_mail.txt";
 
-	    // Set the email subject.
-	    $subject = "New contact from $name";
+			// Build the email content.
+			$email_content = "Name: $name\n";
+			$email_content .= "Email: $email\n\n";
+			$email_content .= "Message:\n$message\n";
 
-	    // Build the email content.
-	    $email_content = "Name: $name\n";
-	    $email_content .= "Email: $email\n\n";
-	    $email_content .= "Message:\n$message\n";
+			file_put_contents($file, $email_content, FILE_APPEND | LOCK_EX);
 
-	    // Build the email headers.
-	    $email_headers = "From: $name <$email>";
-
-	    // Send the email.
-	    if (mail($recipient, $subject, $email_content)) {
-	        // Set a 200 (okay) response code.
-	        http_response_code(200);
-	        echo "Thank You! Your message has been sent.";
-	        die();
-	    } else {
-	        // Set a 500 (internal server error) response code.
-	        http_response_code(500);
-	        echo "Oops! Something went wrong and we couldn't send your message. ". $recipient .", ". $subject .", ". $email_content .", ". $email_headers .".";
-	        die();
-	    }
-
+			// Set a 200 (okay) response code.
+			http_response_code(200);
+			echo "Thank You! Your message has been sent.";
+			die();
+		}
 	} else {
-	    // Not a POST request, set a 403 (forbidden) response code.
-	    http_response_code(403);
-	    echo "There was a problem with your submission, please try again.";
-	    die();
+		// Not a POST request, set a 403 (forbidden) response code.
+		http_response_code(403);
+		echo "There was a problem with your submission, please try again.";
+		die();
 	}
 }
-
 
 function form_shortcode( $atts ) {
 
