2f296e35af3c76c9c5d3a9d7afbd7cc8d1383ebc
Jesse Dillman <JDillman@bibleleague.org>
2015-09-24 16:13:49 -0500

    gulp test

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp'),
+
 	uglify = require('gulp-uglify');
 
 gulp.task('default', function () {
 
 	gulp.src('js/*.js')
+
 	.pipe(uglify())
+
 	.pipe(gulp.dest('js/min'));
 });
