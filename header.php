f5e72b23b45023fc45a71e30ea1150799ba473b3
Jesse Dillman <jesse@usmangroup.com>
2016-09-17 19:44:53 -0400

    site title

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width">
-	<title><?php wp_title(); ?></title>
+	<title><?php bloginfo('name'); wp_title( '|', true, 'right' ); ?></title>
 	<?php 
 		wp_head(); 
 	?>
