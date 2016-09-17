b74557d2b1c43b1b8f4797efd99c6b119b4a228b
Jesse Dillman <jesse@usmangroup.com>
2016-09-17 19:11:59 -0400

    new portfolio page

--- category-work.php
+++ category-work.php
@@ -50,29 +50,6 @@
 				<?php wp_reset_query(); ?>
 			</div>
 
-
-			<div class="post-container">
-
-				<h2>Design</h2>
-
-				<?php query_posts('cat=16'); ?>
-
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php $featuredimage = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
-
-					<img class="work-thumbnail" id="<?php echo $post->post_name; ?>-thumbnail" src="<?php echo $featuredimage; ?>">
-
-					<div class="work-description" id="<?php echo $post->post_name;?>-description" style="display:none;">
-						
-						<div class="exit"></div>
-						<h2><?php the_title(); ?></h2>
-						<?php the_content(); ?>
-					</div>
-				<?php endwhile; ?>
-				<?php wp_reset_query(); ?>
-			</div>
-
 			<img class="full-size-work animate" src="/wp-content/uploads/2015/09/ptf-holland.jpg">
 		</div>
 
