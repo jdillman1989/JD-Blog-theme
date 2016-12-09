58e457f60bed743d888fb4b563df363cf674ed5b
Jesse Dillman <jesse@jdillman.com>
2016-12-09 16:13:27 -0600

    new deployment system

---
+++ bin/config.sh
@@ -0,0 +1,4 @@
+STAGING='jdillman';
+WEBROOT='/var/www/$STAGING';
+PUBLIC='$WEBROOT/public_html';
+SERVER='jesse@jdillman.com';
--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,43 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    newer = require('gulp-newer'),
+    globbing = require('gulp-css-globbing'),
+    cmq = require('gulp-combine-mq'),
 
-var uglify = require('gulp-uglify');
-
-var uglifycss = require('gulp-uglifycss');
-
-var imagemin = require('gulp-imagemin');
-
-gulp.task('default', function () {
-
-	gulp.src('./js/*.js')
-
-	.pipe(uglify())
-
-	.pipe(gulp.dest('./js/min'));
-
-	gulp.src('./sass/*.css')
-
-	.pipe(uglifycss())
-
-	.pipe(gulp.dest('./'));
-
-	gulp.src('./images/*')
-
-	.pipe(imagemin())
-
-	.pipe(gulp.dest('./images'));
+// ROOT TASKS // ---------------------------------------------------------
+// Main style task
+gulp.task('css', function() {
+  return gulp.src('styles/application.scss')
+    .pipe(globbing({extensions: '.scss'}))
+    .pipe(sass())
+    .on('error', handleError)
+    .pipe(cmq()) // combine all @media queries into the page base
+    .pipe(autoprefixer({cascade: false})) // auto prefix
+    .pipe(gulp.dest('wp-content/themes/jadle/assets/css'));
 });
+
+// Main Javascript task
+gulp.task('js', function() {
+  return gulp.src('scripts/**/*.js')
+    .pipe(newer('wp-content/themes/jadle/assets/js'))
+    .pipe(uglify())
+    .on('error', handleError)
+    .pipe(gulp.dest('wp-content/themes/jadle/assets/js'));
+});
+
+// FUNCTIONS // ----------------------------------------------------
+// Initial start function
+gulp.task('start', function() {
+  gulp.start('js', 'css');
+});
+
+// Default function
+gulp.task('default', ['start']);
+
+// Error reporting function
+function handleError(err) {
+  console.log(err.toString());
+  this.emit('end');
+}
